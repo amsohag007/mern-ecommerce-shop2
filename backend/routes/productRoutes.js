20d3cca396fbedaeab6686c9cbe2e28406a700ea
amsohag007 <amsohag007@gmail.com>
2020-11-25 01:49:26 +0600

    productRelatedlist routes and controllers,,, products search routes,controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,6 +8,10 @@ import {
   deleteProduct,
   updateProduct,
   productList,
+  productRelatedlist,
+  productCategoryList,
+  searchProducts,
+  productPhoto,
 } from "../controllers/productControllers.js";
 import {
   requireSignin,
@@ -40,6 +44,10 @@ router.put(
 );
 
 router.get("/products", productList);
+router.get("/products/related/:productId", productRelatedlist);
+router.get("/products/categories", productCategoryList);
+router.post("/products/search", searchProducts); //retun a list of products
+router.get("/product/photo/:productId", productPhoto);
 
 router.param("userId", userById);
 router.param("productId", productById);
