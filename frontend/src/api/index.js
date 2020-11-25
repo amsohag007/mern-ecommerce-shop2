6d0cefff8cc4339a47941b5302bb0d2b70fe98cc
amsohag007 <amsohag007@gmail.com>
2020-11-26 02:18:42 +0600

    signin, save token on local storage,signout

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -19,4 +19,53 @@ const signup = (user) => {
     });
 };
 
-export { signup };
+const signin = (user) => {
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const authenticate = (data, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+const signout = (next) => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+    return fetch(`${API}/signout`, {
+      method: "GET",
+    })
+      .then((response) => {
+        console.log("signout", response);
+      })
+      .catch((err) => console.log(err));
+  }
+};
+
+const isAuthenticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
+
+export { signup, signin, authenticate, signout, isAuthenticated };
