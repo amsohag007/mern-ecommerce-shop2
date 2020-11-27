83939eaa5c9623f7971e6da93b29a987c5df48c0
amsohag007 <amsohag007@gmail.com>
2020-11-27 23:57:32 +0600

    Add product image to card

--- frontend/src/core/Card.js
+++ frontend/src/core/Card.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
-// import ShowImage from './ShowImage';
+import ShowImage from "./ShowImage";
 // import moment from 'moment';
 
 const Card = ({ product }) => {
@@ -8,6 +8,7 @@ const Card = ({ product }) => {
     <div className="card">
       <div className="card-header">{product.name}</div>
       <div className="card-body">
+        <ShowImage item={product} url="product" />
         <p className="card-p  mt-2">{product.description.substring(0, 100)} </p>
         <p className="card-p black-10">$ {product.price}</p>
         <p className="black-9">
