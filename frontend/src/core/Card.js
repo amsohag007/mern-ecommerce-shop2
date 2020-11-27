4b8b910dfc8af6606bd5a74623c78248ec69e0e4
amsohag007 <amsohag007@gmail.com>
2020-11-27 23:50:48 +0600

    sort by sell and arrival to home.js, create product card

---
+++ frontend/src/core/Card.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+// import ShowImage from './ShowImage';
+// import moment from 'moment';
+
+const Card = ({ product }) => {
+  return (
+    <div className="card">
+      <div className="card-header">{product.name}</div>
+      <div className="card-body">
+        <p className="card-p  mt-2">{product.description.substring(0, 100)} </p>
+        <p className="card-p black-10">$ {product.price}</p>
+        <p className="black-9">
+          Category: {product.category && product.category.name}
+        </p>
+      </div>
+      <div>
+        <Link to="/">
+          <button className="btn btn-outline-primary mt-2 mb-2">
+            View Product
+          </button>
+        </Link>
+        <button className="btn btn-outline-primary mt-2 mb-2">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
