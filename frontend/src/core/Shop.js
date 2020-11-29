67ce874a1b6cd8e5209557ffdec279bfcd19027f
amsohag007 <amsohag007@gmail.com>
2020-11-29 23:44:43 +0600

    load categories in sidebar by checkbox

--- frontend/src/core/Shop.js
+++ frontend/src/core/Shop.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { getProducts } from "./apiCore.js";
+import { getCategories } from "./apiCore.js";
+import Checkbox from "./Checkbox";
 
 const Shop = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+
+  const init = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <Layout
       title="Shop Page "
@@ -10,8 +28,15 @@ const Shop = () => {
       className="container-fluid"
     >
       <div className="row">
-        <div className="col-2">Sidebar</div>
-        <div className="col-2">Shop Content</div>
+        <div className="col-4">
+          <h4>Filter by category</h4>
+          <ui>
+            <Checkbox categories={categories} />
+          </ui>
+        </div>
+        <div className="col-8">
+          <h4>Content</h4>
+        </div>
       </div>
     </Layout>
   );
